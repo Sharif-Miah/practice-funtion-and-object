9d092e0638172a35e248197efaab1bebc3ffd8fe
farhan sharif <farhansharif1410@gmail.com>
2022-08-04 21:08:36 -0230

    object practice

---
+++ object.js
@@ -0,0 +1,21 @@
+
+let myProfile = {
+    name: 'Farhan Sharif',
+    age: 22,
+    profesiton: 'Javascript Developer',
+    add: {
+        home: 'Bonaid',
+        postOffice: 'Amdiya',
+        district: 'Narsingdi',
+        country: 'Bangladesh'
+    }
+}
+
+// console.log(myProfile);
+// console.log(myProfile.age);
+// console.log(myProfile.name);
+// console.log(myProfile.profesiton);
+// console.log(myProfile.add);
+// console.log(myProfile.add.home);
+// console.log(myProfile.add.district);
+console.log(myProfile.add.country);
