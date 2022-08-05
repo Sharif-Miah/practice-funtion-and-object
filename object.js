88800c8e71e09bc5878d43aba8e39d9fc715329c
farhan sharif <farhansharif1410@gmail.com>
2022-08-05 09:30:37 -0230

    added practice

--- object.js
+++ object.js
@@ -20,11 +20,52 @@ let myProfile = {
 // console.log(myProfile.add.district);
 // console.log(myProfile.add.country);
 
-let key = Object.keys(myProfile)
-console.log(key);
+// let key = Object.keys(myProfile)
+// console.log(key);
 
-let addKey = Object.keys(myProfile.add)
-console.log(addKey);
+// let addKey = Object.keys(myProfile.add)
+// console.log(addKey);
 
-let value = Object.values(myProfile)
-console.log(value)
+// let value = Object.values(myProfile)
+// console.log(value)
+
+
+let shopingCard = {
+    books: 5,
+    pen: 6,
+    khata: 3,
+    mouse: 1,
+    keybord: 2,
+    monitor: 1,
+    pc: 1,
+    mobileUse: 4
+}
+
+var itemsValeu = 'keybord';
+itemMan = shopingCard[itemsValeu];
+console.log(itemsValeu, itemMan);
+
+// var buysItems = shopingCard;
+// console.log(buysItems);
+
+// var buysItems1 = shopingCard.mobileUse;
+// console.log(buysItems1);
+
+// var buysItems2 = shopingCard['keybord'];
+// console.log(buysItems2);
+
+// var buysItems3 = shopingCard['keybord'];
+// console.log(buysItems3);
+
+var buysItems4 = Object.keys(shopingCard)
+// console.log(buysItems4);
+// var buysItems5 = Object.values(shopingCard)
+// console.log(buysItems5);
+
+// for (let i = 0; i < buysItems4.length; i++) {
+//     let getItems = buysItems4[i];
+//     return getItems;
+// }
+
+// let items = buysItems4[i];
+// console.log(items);
